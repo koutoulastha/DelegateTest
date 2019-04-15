388cdbdcdee375bcb3cb76889b51508409c62062
Thanos Koutoulas <koutoulastha@hotmail.com>
2019-04-15 23:12:05 +0300

    Add project files.

---
+++ DelegateTest/ChildForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegateTest
+{
+     public partial class ChildForm : Form
+     {
+          public delegate void button_click(string text);
+
+          public button_click button_Click;
+
+          public ChildForm()
+          {
+               InitializeComponent();
+          }
+
+          private void button1_Click(object sender, EventArgs e)
+          {
+               button_Click("Delegate works");
+          }
+     }
+}
---
+++ DelegateTest/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegateTest
+{
+     public partial class MainForm : Form
+     {
+          private ChildForm ChildForm;
+
+          public MainForm()
+          {
+               InitializeComponent();
+          }
+
+          private void button1_Click(object sender, EventArgs e)
+          {
+               ChildForm = new ChildForm();
+               ChildForm.button_Click += Changetext;
+               ChildForm.ShowDialog();
+          }
+
+          public void Changetext(string text)
+          {
+               this.textBox1.Text = text;
+          }
+     }
+}
